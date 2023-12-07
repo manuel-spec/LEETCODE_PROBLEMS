e9077b996648aedf961ba9ff2aec9d4799a98453
manuel-spec <amaniase084@gmail.com>
2023-12-07 09:01:39 +0300

    Final

--- Two Sum/main.py
+++ Two Sum/main.py
@@ -1,21 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        nums = [-x for x in nums]
-        target = -1 * target
-        sum = 0
-        result = []
-        pointers = [0, len(nums)-1]
-        for i in range(len(nums)):
-            sum += nums[i]
-        for j in range(len(nums)):
-            if nums[pointers[0]] + nums[pointers[1]] > target:
-                pointers[1] -= 1
-            elif nums[pointers[0]] + nums[pointers[1]] < target:
-                pointers[1] = len(nums) - 1
-                pointers[0] += 1
-            else:
-                return [pointers[0], pointers[1]]
+        num_dict = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
 
 
 c = Solution()
-print(c.twoSum([-1, -2, -3, -4, 10], 8))
+print(c.twoSum([3, 2, 4], 6))
